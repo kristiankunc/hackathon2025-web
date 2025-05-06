e5dcaa54728f668228e50cc2902f1f3651508190
Ilja Novak <ilya496>
2025-05-06 20:39:52 +0200

    add button

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,8 +1,10 @@
 <script>
+	import Button from "$lib/components/button.svelte";
+
 	let appName = "Hackathon 2025";
 </script>
 
 <header class="navbar">
 	<div class="navbar__title">{appName}</div>
-	<button class="navbar__login-button">Login</button>
+	<Button type="button">Login</Button>
 </header>
