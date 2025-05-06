3e436b9d56538be1be99c8f6cb936b7a2576082e
Milan Tuček <105244731+ClydeEmtea@users.noreply.github.com>
2025-05-06 18:32:29 +0200

    code editor

---
+++ src/lib/components/panels/codeEditor.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import hljs from 'highlight.js/lib/core';
+	import python from 'highlight.js/lib/languages/python';
+	import 'highlight.js/styles/github-dark.css';
+
+	import { onMount } from 'svelte';
+
+	export let code = '';
+
+	let codeElement: HTMLElement | null = null;
+
+	onMount(() => {
+		hljs.registerLanguage('python', python);
+		hljs.highlightElement(codeElement!);
+	});
+
+	$: if (codeElement && code) {
+		hljs.highlightElement(codeElement);
+	}
+</script>
+
+<pre><code bind:this={codeElement} class="language-python">{code}</code></pre>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,18 @@
 <script>
 	import PanelContainer from "$lib/components/PanelContainer.svelte";
+	import CodeEditor from "$lib/components/panels/codeEditor.svelte";
+
+	let code = `print("Hello World")`;
 </script>
 
 <PanelContainer>
-	<div slot="left">Left Control</div>
-	<div slot="middle">Middle Control</div>
-	<div slot="right">Right Control</div>
+	<div slot="left"><button on:click={() => code = `print("no hello for you")`}>Change Code</button></div>
+	<div slot="middle">
+		{#key code}
+			<CodeEditor {code} />
+		{/key}
+	</div>
+	<div slot="right">
+		Right Control
+	</div>
 </PanelContainer>
