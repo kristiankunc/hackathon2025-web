90d3ee717d105ff66b12563e1c31445262a4db0e
Kristián Kunc <kristian.kunc@gmail.com>
2025-05-06 20:59:08 +0200

    add basic pyscript

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,6 @@
 	import Navbar from "$lib/components/navbar.svelte";
 </script>
 
-<svelte:head>
-	<script type="module" src="https://pyscript.net/releases/2025.3.1/core.js"></script>
-</svelte:head>
-
 <Navbar />
 
 <main class="container">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -61,6 +61,17 @@ for num in range(lower, upper + 1):
 `;
 </script>
 
+<svelte:head>
+	<link rel="stylesheet" href="https://pyscript.net/releases/2025.3.1/core.css" />
+	<script type="module" src="https://pyscript.net/releases/2025.3.1/core.js"></script>
+</svelte:head>
+
+<py-script>
+from datetime import datetime
+now = datetime.now()
+print(now.strftime("%m/%d/%Y, %H:%M:%S"))
+</py-script>
+
 <PanelContainer>
 	<div slot="left"><button on:click={() => (code = `print("no hello for you")`)}>Change Code</button></div>
 	<div slot="middle">
