b7e007c3f90707fa40f3add295105526f9fc13ce
Jakub Hanslík <jakub@hanslikovi.com>
2025-05-06 20:53:47 +0200

    Python setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 	import Navbar from "$lib/components/navbar.svelte";
 </script>
 
+<svelte:head>
+	<script type="module" src="https://pyscript.net/releases/2025.3.1/core.js"></script>
+</svelte:head>
+
 <Navbar />
 
 <main class="container">
