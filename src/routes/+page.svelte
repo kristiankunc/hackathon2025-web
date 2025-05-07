f0db00c48c2668984e64473df51996d9c17c3221
Jakub Hanslík <jakub@hanslikovi.com>
2025-05-07 14:55:55 +0200

    a

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,29 +30,8 @@ for num in range(lower, upper + 1):
 </svelte:head>
 
 <!-- prettier-ignore -->
-<py-script style="display: none;">
-import pyodide
-import js
-    
-class myClass():
-    def __init__(self):
-        self.add_one_proxy = pyodide.ffi.create_proxy(self.add_one)
-        js.document.getElementById('mybutton').addEventListener("click", self.add_one_proxy)
-        
-    def add_one(self, pointerEventObj):
-        """Callback from JS when a button is clicked"""
-        btn_elem = pointerEventObj.currentTarget
-        data = btn_elem.getAttribute('data-value')
-        js.document.getElementById('resultdiv').innerHTML = str(int(data)+1)
-        
-    def shutdown(self):
-        self.add_one_proxy.destroy()
-    
-klass = myClass()
-# Make sure in the program to keep holding a reference of klass ...
-# do the whole program here
-# When all done, call klass.shutdown()
-</py-script>
+<py-script src="./src/lib/python_modules/test.py"></py-script>
+
 
 <PanelContainer>
 	<div slot="left"><iframe bind:this={iframe} title="Game window" src={env.PUBLIC_UNITY_INSTANCE_URL} class="h-lvh w-full"></iframe></div>
@@ -63,7 +42,5 @@ klass = myClass()
 	</div>
 	<div slot="right">
 		<Chat />
-		<button id="mybutton" data-value="12" aria-label="Add one">Click Me</button>
-		<div id="resultdiv">calculated data will come here on button click</div>
 	</div>
 </PanelContainer>
