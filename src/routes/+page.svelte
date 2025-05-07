d2194cee8ed3cd1ed12efdeecd741c93f334d52e
Jakub Hanslík <jakub@hanslikovi.com>
2025-05-07 12:26:05 +0200

    Pyscript communication with javascript

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -68,29 +68,29 @@ for num in range(lower, upper + 1):
 
 <!-- prettier-ignore -->
 <py-script style="display: none;">
-from pyscript import document
-from pyscript import window
-
-def on_load(event):
-    print("Page loaded")
+import pyodide
+import js
     
-window.addEventListener("load", on_load)
-
-def forward():
-    print("forward")
-
-def backward():
-    print("backward")
-
-def left():
-    print("left")
+class myClass():
+    def __init__(self):
+        self.add_one_proxy = pyodide.ffi.create_proxy(self.add_one)
+        js.document.getElementById('mybutton').addEventListener("click", self.add_one_proxy)
+        
+    def add_one(self, pointerEventObj):
+        """Callback from JS when a button is clicked"""
+        btn_elem = pointerEventObj.currentTarget
+        data = btn_elem.getAttribute('data-value')
+        js.document.getElementById('resultdiv').innerHTML = str(int(data)+1)
+        
+    def shutdown(self):
+        self.add_one_proxy.destroy()
+    
+klass = myClass()
+# Make sure in the program to keep holding a reference of klass ...
+# do the whole program here
+# When all done, call klass.shutdown()
 </py-script>
-
-<!-- prettier-ignore -->
-<py-script style="display: none;">
-forward()
-backward()
-</py-script>
+    
 
 <PanelContainer>
 	<div slot="left"><button on:click={() => (code = `print("no hello for you")`)}>Change Code</button></div>
@@ -101,5 +101,7 @@ backward()
 	</div>
 	<div slot="right">
 		<Chat />
+        <button id="mybutton" data-value="12" aria-label="Add one">Click Me</button>
+        <div id="resultdiv">calculated data will come here on button click</div>
 	</div>
 </PanelContainer>
