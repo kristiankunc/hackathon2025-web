99b193aa474ebf6d5ad8747bd633c3c5ebb19cde
Ilja Novak <ilya496>
2025-05-08 20:29:34 +0200

    show warning if screen is too small

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
 	import "../styles/styles.scss";
 	import Navbar from "$lib/components/navbar.svelte";
+	import { onMount } from "svelte";
+
+	const threshold = 1024;
+	let isTooSmall = false;
+
+	function checkScreenSize() {
+		isTooSmall = window.innerWidth < threshold;
+	}
+
+	onMount(() => {
+		checkScreenSize();
+		window.addEventListener("resize", checkScreenSize);
+		return () => window.removeEventListener("resize", checkScreenSize);
+	});
 </script>
 
 <Navbar />
 
-<main class="container">
-	<slot />
-</main>
+{#if isTooSmall}
+	<div class="screen-warning">This app is best viewed on a larger screen. Please resize your window or use a desktop.</div>
+{:else}
+	<main class="container">
+		<slot />
+	</main>
+{/if}
