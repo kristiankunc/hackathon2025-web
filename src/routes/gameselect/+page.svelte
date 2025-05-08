4cd87a7ade689536a1dd7cafeefb07192ce3e34c
Kristián Kunc <kristian.kunc@gmail.com>
2025-05-08 19:19:56 +0200

    Kristn cleanup (#7)
    * move openai api endpoint
    
    * move game
    
    * ditch prisma
    
    * remove stupid file

--- src/routes/gameselect/+page.svelte
+++ src/routes/gameselect/+page.svelte
@@ -2,4 +2,4 @@
 	import Table from "$lib/components/ui/table.svelte";
 </script>
 
-<Table columns={["Level", "Name", "Score"]} rows={[{ Level: 1, Name: "Level 1", Score: "95%", link: "/gameselect/" + 1 }]} />
+<Table columns={["Level", "Name", "Score"]} rows={[{ Level: 1, Name: "Level 1", Score: "95%", link: `/game/${1}` }]} />
